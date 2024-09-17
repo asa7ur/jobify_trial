f8765e9b30d0fd6ec54f7f0d498070f600170a2b
Garik Asatryan <asa7ur@gmail.com>
2024-09-17 22:34:28 +0200

    Basic server

--- server.js
+++ server.js
@@ -1,10 +1,36 @@
-import express from "express"
+import * as dotenv from 'dotenv'
+dotenv.config()
+import express from 'express'
 const app = express()
+import morgan from 'morgan'
+import { nanoid } from 'nanoid'
+
+let jobs = [
+  { id: nanoid(), company: 'apple', position: 'front-end' },
+  { id: nanoid(), company: 'google', position: 'back-end' },
+]
+
+if (process.env.NODE_ENV === 'development') {
+  app.use(morgan('dev'))
+}
+
+app.use(express.json())
 
 app.get('/', (req, res) => {
-    res.send('Hello world!')
+  res.send('Hello world!')
 })
 
+app.post('/', (req, res) => {
+  console.log(req)
+  res.json({ message: 'data received', data: req.body })
+})
+
+app.get('/api/v1/jobs', (req, res) => {
+  res.status(200).json({jobs})
+})
+
+const port = process.env.PORT || 5100
+
 app.listen(5100, () => {
-    console.log('server running...');
+  console.log(`server running on PORT ${port}`)
 })
