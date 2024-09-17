c7f7cd261138491e6037b79c34b29a5707f54a82
Garik Asatryan <asa7ur@gmail.com>
2024-09-17 23:44:42 +0200

    Added CRUD to the server

--- server.js
+++ server.js
@@ -25,8 +25,70 @@ app.post('/', (req, res) => {
   res.json({ message: 'data received', data: req.body })
 })
 
+// get all jobs
 app.get('/api/v1/jobs', (req, res) => {
-  res.status(200).json({jobs})
+  res.status(200).json({ jobs })
+})
+
+// create a job
+app.post('/api/v1/jobs', (req, res) => {
+  const { company, position } = req.body //req.body is where the JSON data is stored
+  if (!company || !position) {
+    return res.status(400).json({ msg: 'please provide company and position' })
+  }
+  const id = nanoid(10) // an id of 10 characters
+  const job = { id, company, position }
+  jobs.push(job)
+  res.status(201).json({ job })
+})
+
+// get single job
+app.get('/api/v1/jobs/:id', (req, res) => {
+  const { id } = req.params // refers to the route parameters that are part of the URL
+  const job = jobs.find((job) => job.id === id)
+  if (!job) {
+    return res.status(404).json({ msg: `no job with id ${id}` })
+  }
+  res.status(200).json({ job })
+})
+
+// edit job
+app.patch('/api/v1/jobs/:id', (req, res) => {
+  const { company, position } = req.body
+  if (!company || !position) {
+    return res.status(400).json({ msg: 'please provide company and position' })
+  }
+  const { id } = req.params
+  const job = jobs.find((job) => job.id === id)
+  if (!job) {
+    return res.status(404).json({ msg: `no job with id ${id}` })
+  }
+  job.company = company
+  job.position = position
+
+  res.status(200).json({ msg: 'job modified', job })
+})
+
+// delete job
+app.delete('/api/v1/jobs/:id', (req, res) => {
+  const { id } = req.params
+  const job = jobs.find((job) => job.id === id)
+  if (!job) {
+    return res.status(404).json({ msg: `no job with id ${id}` })
+  }
+  const newJobs = jobs.filter((job) => job.id !== id)
+  jobs = newJobs
+
+  res.status(200).json({ msg: 'job deleted' })
+})
+
+app.use('*', (req, res) => {
+  res.status(404).json({ msg: 'not found' })
+})
+
+app.use((err, req, res, next) => {
+  console.log(err)
+  res.status(500).json({msg: 'something went wrong'})
 })
 
 const port = process.env.PORT || 5100
