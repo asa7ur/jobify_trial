ab31ce311eba48ff7cb44221cecdb7062c028eea
Garik Asatryan <asa7ur@gmail.com>
2024-10-01 10:48:14 +0200

    added all jobs page

--- client/src/pages/AllJobs.jsx
+++ client/src/pages/AllJobs.jsx
@@ -4,7 +4,28 @@ import customFetch from '../utils/customFetch'
 import { useLoaderData } from 'react-router-dom'
 import { useContext, createContext } from 'react'
 
-const AllJobs = () => {
-  return <div>AllJobs</div>
+export const loader = async () => {
+  try {
+    const {data} = await customFetch.get('/jobs')
+    return {data}
+  } catch (error) {
+    toast.error(error?.response?.data?.msg)
+    return error
+  }
 }
+
+const AllJobsContext = createContext()
+const AllJobs = () => {
+  const {data} = useLoaderData()
+  
+  return (
+    <AllJobsContext.Provider value={{data}}>
+      <SearchContainer />
+      <JobsContainer />
+    </AllJobsContext.Provider>
+  )
+}
+
+export const useAllJobsContext = () => useContext(AllJobsContext)
+
 export default AllJobs
