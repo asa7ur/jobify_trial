9a09893fec8ecf1c0644a0acf275364ded569cc0
Garik Asatryan <asa7ur@gmail.com>
2024-10-02 11:02:52 +0200

    minor changes

--- client/src/pages/AddJob.jsx
+++ client/src/pages/AddJob.jsx
@@ -1,8 +1,12 @@
 import { FormRow, FormRowSelect } from '../components'
 import Wrapper from '../assets/wrappers/DashboardFormPage'
-import { useOutletContext } from 'react-router-dom'
 import { JOB_STATUS, JOB_TYPE } from '../../../utils/constants'
-import { Form, useNavigation, redirect } from 'react-router-dom'
+import {
+  Form,
+  useNavigation,
+  redirect,
+  useOutletContext,
+} from 'react-router-dom'
 import { toast } from 'react-toastify'
 import customFetch from '../utils/customFetch'
 
