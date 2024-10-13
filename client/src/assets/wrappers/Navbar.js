2e745eba1d73952af923fc46cf7b662f96aa7259
Garik Asatryan <asa7ur@gmail.com>
2024-10-13 20:03:57 +0200

    Minor fixes

--- client/src/assets/wrappers/Navbar.js
+++ client/src/assets/wrappers/Navbar.js
@@ -1,4 +1,4 @@
-import styled from 'styled-components';
+import styled from 'styled-components'
 
 const Wrapper = styled.nav`
   height: var(--nav-height);
@@ -47,5 +47,5 @@ const Wrapper = styled.nav`
       display: block;
     }
   }
-`;
-export default Wrapper;
+`
+export default Wrapper
